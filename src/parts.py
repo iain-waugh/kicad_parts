34583cccb0846444a921027d7ce4c8b84897dddc
Iain Waugh <iwaugh@gmail.com>
2025-02-03 19:46:38 +1100

    Clean-up parts.py while reconsidering how parts are generated

--- src/parts.py
+++ src/parts.py
@@ -37,8 +37,71 @@ class Series:
       ]
     # fmt:on
 
-    def __init__(self, series_start_number = 0):
+    def __init__(self, series_string, series_start_number=0):
+        self.series_string = series_string
         self.part_id_num = series_start_number
+
+    def str_mult(self, value, mult):
+        """Multiply a numeric string by 1, 10 or 100."""
+        l = len(value)
+        if value[1] != ".":
+            raise ValueError(
+                "Only numbers of the form x.y or x.yy are allowed; you need a decimal point"
+            )
+        if mult == 1:
+            return value
+        elif mult == 10 and l == 3:
+            return value[0] + value[2]
+        elif mult == 10 and l == 4:
+            return value[0] + value[2] + "." + value[3]
+        elif mult == 100 and l == 3:
+            return value[0] + value[2] + "0"
+        elif mult == 100 and l == 4:
+            return value[0] + value[2:]
+        else:
+            raise ValueError("Only numbers of the form x.y or x.yy are allowed; got: ", value)
+
+    def str2numeric(self, value, schem=True):
+        if schem:
+            value = self.schem2text(value)
+
+        if value[-1] == "k":
+            return float(value[:-1]) * 1000
+        elif value[-1] == "M":
+            return float(value[:-1]) * 1000000
+        return float(value)
+
+    def gen_range(self, min_val, max_val, value_list):
+        """
+        Generate a list of values at different magnitudes, based on a supplied set of values
+
+        min_val : a schematic value string, like "10R"
+        max_val : a schematic value string, like "10M""
+        value_list : the e24 or e96 list, or a custom list
+
+        This is really awkward because as soon as you start multiplying floating point
+        numbers, you hit rounding precision and you get 8.19999999 instead of 8.2 (for example).
+
+        So...  Do the maths in strings (!)  It's only x10, x100, anyway.
+        """
+        multipliers = [1, 10, 100]
+        mags = ["R", "k", "M"]
+        min_val = self.str2numeric(min_val, schem=True)
+        max_val = self.str2numeric(max_val, schem=True)
+        result = []
+
+        for mag in mags:
+            for m in multipliers:
+                for v in value_list:
+                    text_num = self.str_mult(v, m)
+                    if "." in text_num:
+                        sch_num = re.sub(r"\.", mag, text_num)
+                    else:
+                        sch_num = text_num + mag
+                    n = self.str2numeric(sch_num)
+                    if n >= min_val and n <= max_val:
+                        result.append(sch_num)
+        return result
 
 
 class Part:
@@ -85,40 +148,10 @@ class Part:
             # If the last character is "k" or "M", then leave it
             new_val = value
         else:
-            new_val = re.sub(r"(\d+)([RkM])(\d+)", r"\1.\3\2", value)
+            new_val = re.sub(r"(\d+)([RkMmunpfa])(\d+)", r"\1.\3\2", value)
             if new_val[-1] == "R":
                 new_val = new_val[:-1]
         return new_val
-
-    def str2numeric(self, value, schem=True):
-        if schem:
-            value = self.schem2text(value)
-
-        if value[-1] == "k":
-            return float(value[:-1]) * 1000
-        elif value[-1] == "M":
-            return float(value[:-1]) * 1000000
-        return float(value)
-
-    def str_mult(self, value, mult):
-        """Multiply a numeric string by 1, 10 or 100."""
-        l = len(value)
-        if value[1] != ".":
-            raise ValueError(
-                "Only numbers of the form x.y or x.yy are allowed; you need a decimal point"
-            )
-        if mult == 1:
-            return value
-        elif mult == 10 and l == 3:
-            return value[0] + value[2]
-        elif mult == 10 and l == 4:
-            return value[0] + value[2] + "." + value[3]
-        elif mult == 100 and l == 3:
-            return value[0] + value[2] + "0"
-        elif mult == 100 and l == 4:
-            return value[0] + value[2:]
-        else:
-            raise ValueError("Only numbers of the form x.y or x.yy are allowed; got: ", value)
 
 
 class Resistor(Part):
@@ -178,16 +211,16 @@ class Resistor(Part):
         # Use standard KiCad SMD resistor footprints
         footprints_table = {
             #    "0075" : "Resistor_SMD:R_unavailable",
-            "0100": "Resistor_SMD:R_01005_0402Metric;R_01005_0402Metric_Pad0.57x0.30mm_HandSolder",
-            "0201": "Resistor_SMD:R_0201_0603Metric;R_0201_0603Metric_Pad0.64x0.40mm_HandSolder",
-            "0402": "Resistor_SMD:R_0402_1005Metric;R_0402_1005Metric_Pad0.72x0.64mm_HandSolder",
-            "0603": "Resistor_SMD:R_0603_1608Metric;R_0603_1608Metric_Pad0.98x0.95mm_HandSolder",
-            "0805": "Resistor_SMD:R_0805_2012Metric;R_0805_2012Metric_Pad1.20x1.40mm_HandSolder",
-            "1206": "Resistor_SMD:R_1206_3216Metric;R_1206_3216Metric_Pad1.30x1.75mm_HandSolder",
-            "1210": "Resistor_SMD:R_1210_3225Metric;R_1210_3225Metric_Pad1.30x2.65mm_HandSolder",
-            "1218": "Resistor_SMD:R_1218_3246Metric;R_1218_3246Metric_Pad1.22x4.75mm_HandSolder",
-            "2010": "Resistor_SMD:R_2010_5025Metric;R_2010_5025Metric_Pad1.40x2.65mm_HandSolder",
-            "2512": "Resistor_SMD:R_2512_6332Metric;R_2512_6332Metric_Pad1.40x3.35mm_HandSolder",
+            "0100": "Resistor_SMD:R_01005_0402Metric;Resistor_SMD:R_01005_0402Metric_Pad0.57x0.30mm_HandSolder",
+            "0201": "Resistor_SMD:R_0201_0603Metric;Resistor_SMD:R_0201_0603Metric_Pad0.64x0.40mm_HandSolder",
+            "0402": "Resistor_SMD:R_0402_1005Metric;Resistor_SMD:R_0402_1005Metric_Pad0.72x0.64mm_HandSolder",
+            "0603": "Resistor_SMD:R_0603_1608Metric;Resistor_SMD:R_0603_1608Metric_Pad0.98x0.95mm_HandSolder",
+            "0805": "Resistor_SMD:R_0805_2012Metric;Resistor_SMD:R_0805_2012Metric_Pad1.20x1.40mm_HandSolder",
+            "1206": "Resistor_SMD:R_1206_3216Metric;Resistor_SMD:R_1206_3216Metric_Pad1.30x1.75mm_HandSolder",
+            "1210": "Resistor_SMD:R_1210_3225Metric;Resistor_SMD:R_1210_3225Metric_Pad1.30x2.65mm_HandSolder",
+            "1218": "Resistor_SMD:R_1218_3246Metric;Resistor_SMD:R_1218_3246Metric_Pad1.22x4.75mm_HandSolder",
+            "2010": "Resistor_SMD:R_2010_5025Metric;Resistor_SMD:R_2010_5025Metric_Pad1.40x2.65mm_HandSolder",
+            "2512": "Resistor_SMD:R_2512_6332Metric;Resistor_SMD:R_2512_6332Metric_Pad1.40x3.35mm_HandSolder",
         }
         self.footprints = footprints_table[package]
         self.manufacturers = "Yageo"
@@ -222,116 +255,6 @@ class Resistor(Part):
 
         code = "RC" + self.package + tol_code + packaging_code + "-" + reel_code + value + "L"
         return code
-
-
-class Capacitor(Part):
-    def __init__(self, value_sch, series, package, power, tol, voltage, min_c, max_c):
-        super().__init__()
-        self.part_prefix = self.part_prefix + "C1"
-        self.value = value_sch
-        self.series = series
-        self.package = package
-        self.power = power
-        self.tol = tol
-        self.voltage = voltage
-        self.min_c = min_c
-        self.max_c = max_c
-        self.RoHS = "OK"
-
-
-class CapacitorChip(Capacitor):
-    def __init__(self):
-        super().__init__()
-
-        self.description = " ".join(
-            ["CAP", "CHIP", self.schem2text(self.value_sch) + " F", self.tol, self.power, self.package]
-        )
-        self.fields = (
-            self.field_pre
-            + ["Value", "Tolerance", "Power", "Package", "Working Voltage", "ESR"]
-            + self.field_post
-        )
-
-        height_table = {
-            "0201": "0.23",
-            "0402": "0.35",
-            "0603": "0.45",
-            "0805": "0.50",
-            "1206": "0.55",
-            "1210": "0.50",
-            "1218": "0.55",
-        }
-        self.height = height_table[self.package]
-
-        # Weight will be rounded up/down to a precision of 1mg
-        #   so values of 0402 and smaller will be 0
-        #   (a through hole via weighs more)
-        weight_table = {
-            "0201": "0.0",  # "0.0002",
-            "0402": "0.0",  # "0.0006",
-            "0603": "0.002",
-            "0805": "0.004",
-            "1206": "0.010",
-            "1210": "0.016",
-            "1218": "0.027",
-        }
-        self.weight = weight_table[self.package]
-        self.symbols = "Passives:C"
-
-        # Use standard KiCad SMD resistor footprints
-        footprints_table = {
-            "0201": "Resistor_SMD:C_0201_0603Metric;C_0201_0603Metric_Pad0.64x0.40mm_HandSolder",
-            "0402": "Resistor_SMD:C_0402_1005Metric;C_0402_1005Metric_Pad0.74x0.62mm_HandSolder",
-            "0603": "Resistor_SMD:C_0603_1608Metric;C_0603_1608Metric_Pad1.08x0.95mm_HandSolder",
-            "0805": "Resistor_SMD:C_0805_2012Metric;C_0805_2012Metric_Pad1.18x1.45mm_HandSolder",
-            "1206": "Resistor_SMD:C_1206_3216Metric;C_1206_3216Metric_Pad1.33x1.80mm_HandSolder",
-            "1210": "Resistor_SMD:C_1210_3225Metric;C_1210_3225Metric_Pad1.33x2.70mm_HandSolder",
-            "1812": "Resistor_SMD:C_1812_4532Metric;C_1812_4532Metric_Pad1.57x3.40mm_HandSolder",
-        }
-        self.footprints = footprints_table[self.package]
-        self.manufacturers = "Yageo"
-        self.mpns = self.avx_code() + ";" + self.kemet_code()
-        self.prices = "100:0.01;20000:0.0003"
-        self.datasheet = "https://www.yageo.com/upload/media/product/products/datasheet/rchip/PYu-RC_Group_51_RoHS_L_12.pdf"
-
-    def avx_code(self):
-        """Return the AVX part number for this resistor."""
-        code = "TODO AVX"
-
-        return code
-
-    def kemet_code(self):
-        """Return the AVX part number for this resistor."""
-        code = "TODO Kemet"
-
-        return code
-
-class CapacitorPolymer(Capacitor):
-    def __init__(self):
-        super().__init__()
-
-class CapacitorTantalum(Capacitor):
-    def __init__(self):
-        super().__init__()
-
-class CapacitorElectrolytic(Capacitor):
-    def __init__(self):
-        super().__init__()
-
-class CapacitorFilm(Capacitor):
-    def __init__(self):
-        super().__init__()
-
-class CapacitorSuper(Capacitor):
-    def __init__(self):
-        super().__init__()
-
-
-class Inductor(Part):
-    def __init__(self, part_id, value_sch):
-        super().__init__(part_id)
-        self.value = value_sch
-
 
 if __name__ == "__main__":
     r = Resistor("1R0", "0402", "1/16W", "1%", "50V", "-55", "155")
